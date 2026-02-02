676b9edbf7f9c0f07367e77dd8a05557ba90be43
bgd <trifan.bogdan09@yahoo.com>
2026-02-02 13:34:25 +0200

    feat: Add CSS styling for login page; refactor Login component structure

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -3,6 +3,7 @@ import { useForm, Controller } from "react-hook-form";
 import ByteStreakLogo from "../../ByteStreak.logo";
 import { colors } from "../../colors";
 import "../../fonts.css";
+import './Login.style.css';
 
 type LoginFormInputs = {
     email: string;
@@ -18,82 +19,34 @@ function LoginPage() {
     } = useForm<LoginFormInputs>();
 
     const onSubmit = (data: LoginFormInputs) => {
-        console.log("Login Data:", data);
         // Handle login logic here
     }
 
     return (
-        <Box 
-            height = {"100vh"} 
-            width = {"100vw"} 
-            display = {"flex"} 
-            justifyContent = {"center"} 
-            alignItems = {"center"}
-            sx={{
-                backgroundColor: `${colors.night}`
-            }}
-        > {/* Fullscreen Centered Box */ }
-            <Box padding={2} border={1} borderRadius={5} borderColor={colors.emerald}> {/* Container Box */ }
-                <Box textAlign = {"center"}> {/* Header Section */ }
+        <Box id="login-page">
+            <Box id="login-container">
+                <Box id="login-header">
                     <ByteStreakLogo size={80} />
-                    <Typography 
-                        variant = "h6"
-                        fontFamily = {"Momo Trust Display"}
-                        sx={{
-                            color: `${colors.white}`
-                        }}
-                        gutterBottom
-                    >
-                        - Login -
+                    <Typography id="login-title">
+                        LOGIN
                     </Typography>
                 </Box>
-                <Box marginTop={5}> {/* Form Section */ }
+                <Box id="login-form-container">
                     <form onSubmit={handleSubmit(onSubmit)}>
                         { /* Email */ }
                         <Controller
                             control = { control }
                             name = "email"
                             render={({ field }) => (
-                                <TextField 
-                                    {...field}
-                                    id = "login-email"
+                                <TextField {...field} 
+                                    className="login-textfield"
                                     label = "Email"
                                     variant = "outlined"
                                     placeholder = "Enter your email"
-                                    margin = "normal"
-                                    fullWidth
-                                    sx={{
-                                        '& .MuiFormLabel-root': {
-                                            color: colors.white
-                                        },
-                                        '& .MuiFormLabel-root .MuiInputLabel-root.Mui-focused': {
-                                            color: colors.white
-                                        },
-                                        '& .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: colors.emerald,
-                                            borderRadius: 0,
-                                            transition: 'border-radius 0.3s ease, border-color 0.3s ease'
-                                        },
-                                        '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: colors.yellow,
-                                            borderRadius: 4,
-                                            transition: 'border-radius 0.3s ease, border-color 0.3s ease'
-                                        },
-                                        '& .MuiInputLabel-root .Mui-focused': {
-                                            color: colors.white
-                                        },
-                                        '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: colors.yellow,
-                                            borderRadius: 4,
-                                            transition: 'border-radius 0.3s ease, border-color 0.3s ease'
-                                        },
-                                        input: {
-                                            color: colors.white
-                                        }
-                                    }}
                                     helperText = {
                                         errors.email ? errors.email.message : ""
                                     }
+                                    fullWidth
                                 />
                             )}
                         />
@@ -109,55 +62,18 @@ function LoginPage() {
                                 <TextField 
                                     {...field}
                                     fullWidth
+                                    className="login-textfield"
                                     type = "password"
-                                    id = "login-password"
                                     label = "Password"
                                     variant = "outlined"
                                     placeholder = "Enter your password"
                                     margin = "normal"
-                                    sx={{
-                                        '& .MuiFormLabel-root': {
-                                            color: colors.white
-                                        },
-                                        '& .MuiFormLabel-root .MuiInputLabel-root.Mui-focused': {
-                                            color: colors.white
-                                        },
-                                        '& .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: colors.emerald,
-                                            borderRadius: 0,
-                                            transition: 'border-radius 0.3s ease, border-color 0.3s ease'
-                                        },
-                                        '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: colors.yellow,
-                                            borderRadius: 4,
-                                            transition: 'border-radius 0.3s ease, border-color 0.3s ease'
-                                        },
-                                        '& .MuiInputLabel-root .Mui-focused': {
-                                            color: colors.white
-                                        },
-                                        '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: colors.yellow,
-                                            borderRadius: 4,
-                                            transition: 'border-radius 0.3s ease, border-color 0.3s ease'
-                                        },
-                                        input: {
-                                            color: colors.white
-                                        }
-                                    }}
                                 />
                             )}
                         />
 
                        
-                        <Button 
-                            fullWidth 
-                            variant = "contained" 
-                            sx={{ 
-                                marginTop: 2, 
-                                fontFamily: "Momo Trust Display", 
-                                backgroundColor: colors.emerald,
-                            }}
-                            type = "submit">
+                        <Button id="login-button" variant = "contained" type = "submit" fullWidth>
                             Login
                         </Button>
                     </form>
