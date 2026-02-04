726a7dd5dc7829b22dcae30067045184306b2c1b
bgd <trifan.bogdan09@yahoo.com>
2026-02-04 23:19:38 +0200

    feat: Refactor coding problems section by replacing CodingProblems component with ProblemsSection and adding new ProblemCard component

--- frontend/src/pages/dashboard-page/Dashboard.tsx
+++ frontend/src/pages/dashboard-page/Dashboard.tsx
@@ -1,14 +1,14 @@
 import { Box } from "@mui/material"
 import "./Dashboard.style.css" 
 import Navbar from "../../components/navbar/Navbar";
-import CodingProblems from "../../components/dashboard/coding-problems-section/CodingProblems";
+import ProblemsSection from "../../features/Dashboard/ProblemsSection/ProblemsSection";
 
 function Dashboard() {
     return (
         <Box id="dashboard-container">
             <Navbar />
             <Box id="dashboard-content">
-                <CodingProblems />
+                <ProblemsSection />
             </Box>
         </Box>
     )
