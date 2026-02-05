b234f575079708ee488338eabaed00eea2b63cea
bgd <trifan.bogdan09@yahoo.com>
2026-02-05 10:23:33 +0200

    feat: Initialize Spring Boot project with Maven Wrapper, configuration files, and basic application structure

---
+++ backend/src/main/java/com/bytestreak/backend/ByteStreakBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bytestreak.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteStreakBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteStreakBackendApplication.class, args);
+	}
+
+}
