46d648c0e5705cdeba531dbabc84be0ece1bcf8d
Bogdan <trifan.bogdan09@yahoo.com>
2026-05-24 16:09:40 +0300

    feat: Implement problem visibility toggle and update related services and UI components

--- backend/src/main/java/com/bytestreak/backend/services/ProblemService.java
+++ backend/src/main/java/com/bytestreak/backend/services/ProblemService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.bytestreak.backend.repositories.ProblemRepository;
 import com.bytestreak.backend.entities.Problem;
 import com.bytestreak.backend.enums.Difficulty;
+import com.bytestreak.backend.enums.Visibility;
 
 import java.util.List;
 
@@ -15,15 +16,15 @@ public class ProblemService {
     @Autowired
     private ProblemRepository problemRepository;
     
-    public List<Problem> getAllProblems(String difficulty) {
+    public List<Problem> getPublicProblems(String difficulty) {
         if (difficulty == null || difficulty.isBlank()) {
-            return problemRepository.findAll();
+            return problemRepository.findByVisibility(Visibility.PUBLIC);
         }
 
         try {
             Difficulty difficultyEnum = Difficulty.valueOf(difficulty.toUpperCase());
             
-            return problemRepository.findByDifficulty(difficultyEnum);     
+            return problemRepository.findByVisibilityAndDifficulty(Visibility.PUBLIC, difficultyEnum);
         } 
         catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("Invalid difficulty level: " + difficulty);
